63e92566e0d5fe9484240b86aeb52cd0e956d2ac
Nelya <nelya.tairova@gmail.com>
2024-12-11 21:15:52 +0300

    тест на удаление контакта

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-
-
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -17,12 +15,10 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
-
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/index.php")
 
     def destroy(self):
         self.wd.quit()
-
 
