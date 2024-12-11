bc9a6eef042cff63b2aea890f499cffa3e9d7463
Nelya <nelya.tairova@gmail.com>
2024-12-11 14:57:01 +0300

    файл conftest создан и дабвлен новый тест на удаление группы

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+from requests import session
+
+from fixture.application import Application
+import pytest
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
--- tests/test_add_new_contact.py
+++ tests/test_add_new_contact.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-
-from fixture.application import Application
 from model.contact import Contact
-import pytest
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_new_contact(app):
     app.session.login("admin", "secret")
