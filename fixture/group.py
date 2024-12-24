65aa218b63638e06eb5727dc2d7100c2f789379e
Nelya <nelya.tairova@gmail.com>
2024-12-24 11:49:09 +0300

    Доработаны тесты для удаления и модификации групптак, чтобы в них удалялся или модифицировался случайно выбранный элемент.

--- fixture/group.py
+++ fixture/group.py
@@ -25,21 +25,25 @@ class GroupHelper:
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
 
-    def delete_first_group(self):
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        #select first group
-        wd.find_element_by_name("selected[]").click()
-        #submit deletion
+        self.select_group_by_index(index)
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
 
-    def edit_first_group(self, group):
+    def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def edit_first_group(self):
+        wd = self.app.wd
+        self.edit_group_by_index(0)
+
+    def edit_group_by_index(self, index, group):
         wd = self.app.wd
         self.open_groups_page()
-        #select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_group_by_index(index)
         #submit deletion
         wd.find_element_by_name("edit").click()
         #fill group form
@@ -48,6 +52,14 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
         self.group_cache = None
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[0].click()
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
 
     def fill_group_form(self, group):
         wd = self.app.wd
--- tests/test_edit_first_group.py
+++ tests/test_edit_first_group.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 from model.group import Group
 
 
@@ -6,11 +8,12 @@ def test_edit_first_group(app):
         app.group.create(Group(name = 'Test_group'))
     old_groups = app.group.get_group_list()
     group = Group("edited_group_name", "edited_test_header", "edited_test_footer")
-    group.id=old_groups[0].id
-    app.group.edit_first_group(group)
+    index = randrange(len(old_groups))
+    group.id=old_groups[index].id
+    app.group.edit_group_by_index(index, group)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 # def test_edit_first_group_name(app):
