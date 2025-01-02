4ed3eca0a181ecd331adabc297779f0a7c350986
Nelya <nelya.tairova@gmail.com>
2025-01-02 13:00:24 +0300

    Параметризованы тесты для создания групп и контактов, чтобы в них использовались случайно сгенерированные строки для свойств создаваемых объектов.

--- model/contact.py
+++ model/contact.py
@@ -33,7 +33,7 @@ class Contact:
         self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
-        return "%s: %s: %s" % (self.firstname, self.lastname, self.id)
+        return "%s: %s: %s" % (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
         return ((self.id is None or other.id is None or self.id == other.id)
--- tests/test_add_new_group.py
+++ tests/test_add_new_group.py
@@ -1,23 +1,31 @@
+import string
+from random import random
+
 from model.group import Group
+import pytest
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation +" "*10 #string.punctuation +
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+test_data = [Group(name="",header= "", footer="")] + [
+             Group(name=random_string("name", 10),
+                   header=random_string("header", 20),
+                   footer=random_string("footer", 20)) for i in range(5)
+]
 
 
-def test_add_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group("test_group", "test_header", "test_footer")
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
+def test_add_group(app, group):
+     old_groups = app.group.get_group_list()
+     app.group.create(group)
+     assert len(old_groups) + 1 == app.group.count()
+     new_groups = app.group.get_group_list()
+     old_groups.append(group)
+     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group("", "", "")
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
