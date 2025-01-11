0d0e2560bf8bf0ee58f0f869d30298b6b8acaf27
Nelya <nelya.tairova@gmail.com>
2025-01-11 17:05:08 +0300

    Переделан тест для модификации групп, чтобы списки объектов загружались из базы данных, а не из пользовательского интерфейса.

--- tests/test_edit_first_contact.py
+++ tests/test_edit_first_contact.py
@@ -7,7 +7,7 @@ def test_edit_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create_new(Contact(firstname="Nelya"))
     old_contacts = app.contact.get_contact_list()
-    contact = Contact("Edited_Nelya_test", "edited_middle_test", "edited_Tairova", "edited_NT", "edited_test_title", "edited_Starpoint",
+    contact = Contact("333Edited_Nelya_test", "edited_middle_test", "edited_Tairova", "edited_NT", "edited_test_title", "edited_Starpoint",
                              "edited_home_address_for_testing 30-1-32", "1234567890", "9876543210", "123654987", "123",
                              "test1_edited@test.com", "test2_edited@test.com", "test3@test.com", "www.kvakva.com", "10", "March",
                              "1900", "1", "February", "2011", '')
--- tests/test_edit_first_group.py
+++ tests/test_edit_first_group.py
@@ -4,34 +4,20 @@ from model.group import Group
 
 def test_edit_first_group(app, db, check_ui):
     if app.group.count() == 0:
-        app.group.create(Group(name = 'Test_group'))
+        app.group.create(Group(name='Test_group'))
     old_groups = db.get_group_list()
-    group_data = Group("edited_group_name1", "edited_test_header1", "edited_test_footer1")
-    #index = randrange(len(old_contacts))
-   # contact.id = old_contacts[index].id
-    #app.contact.edit_contact_by_index(index, contact)
-    group= random.choice(old_groups)
-    print(group)
-    app.group.edit_group_by_id(group.id, group_data)
-    #assert len(old_groups) == app.group.count()
+    group = Group("edited_group_name", "edited_test_header", "edited_test_footer")
+    group_to_edit = random.choice(old_groups)
+    app.group.edit_group_by_id(group_to_edit.id, group)
     new_groups = db.get_group_list()
-    print(group)
-    for index, item in enumerate(old_groups):
-        if item == group:
-            print('da!!!!')
-            old_groups[index] = group_data
-    print(new_groups)
-    print(old_groups)
+    for i in old_groups:
+        if i.id == group_to_edit.id:
+            i.name = group.name
+            i.header = group.header
+            i.footer = group.footer
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
-    #old_groups[index] = group
-   # assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-   # if check_ui:
-       # assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
-# def test_edit_first_group_name(app):
-#     if app.group.count() == 0:
-#         app.group.create(Group(name = 'Test_group'))
-#     old_groups = app.group.get_group_list()
-#     app.group.edit_first_group(Group(name="group_name_updated"))
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) == len(new_groups)
+
