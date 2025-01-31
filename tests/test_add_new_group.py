770c9b7c4c4efe6e4c9bfd60a15e5c02e4909c8a
Nelya <nelya.tairova@gmail.com>
2025-01-31 23:45:10 +0300

    добавлены закомментированный теги алюра

--- tests/test_add_new_group.py
+++ tests/test_add_new_group.py
@@ -1,10 +1,14 @@
+import allure
+import allure_pytest
 
 from model.group import Group
 
-
+#@allure.epic("Creation cases")
 def test_add_group(app, db, json_groups, check_ui):
      group = json_groups
+     #with allure.step('a group list'):
      old_groups = db.get_group_list()
+     #with allure.step('add the group to the list'):
      app.group.create(group)
      assert len(old_groups) + 1 == app.group.count()
      new_groups = db.get_group_list()
