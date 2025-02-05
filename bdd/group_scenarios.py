b422b8f2067abb97c8466a4d58c20d367e3c5ec4
ntairova <38103230+ntairova@users.noreply.github.com>
2025-02-05 20:56:01 +0300

    Update group_scenarios.py

--- bdd/group_scenarios.py
+++ bdd/group_scenarios.py
@@ -1,11 +1,23 @@
 from pytest_bdd import scenario
+from .contact_steps import *
 from .group_steps import *
 
 @scenario("groups.feature", "Add new group")
 def test_add_new_group():
     pass
 
-
 @scenario("groups.feature", "Delete a group")
 def test_delete_group():
+    pass
+
+@scenario("contacts.feature", "Add new contact")
+def test_add_new_contact():
+    pass
+
+@scenario("contacts.feature", "Delete a contact")
+def test_delete_contact():
+    pass
+
+@scenario("contacts.feature", "Modify a contact")
+def test_modify_contact():
     pass
