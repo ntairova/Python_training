6fe41fd2446080e5e51121339b15d156d2b738d4
ntairova <38103230+ntairova@users.noreply.github.com>
2025-02-05 20:39:10 +0300

    Update contact_steps.py

--- bdd/contact_steps.py
+++ bdd/contact_steps.py
@@ -47,7 +47,7 @@ def verify_contact_deleted(db, non_empty_contact_list, random_contact, app, chec
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
                                                                      key=Contact.id_or_max)
 
-@when((parsers.parse("I modify the contact data with {firstname} and {lastname} from the list", target_fixture="modify_contact")
+@when(parsers.parse("I modify the contact data with {firstname} and {lastname} from the list"), target_fixture="modify_contact")
 def modify_contact(app, random_contact, firstname, lastname):
     contact =  Contact(firstname=firstname, lastname=lastname)
     contact.id = random_contact.id
